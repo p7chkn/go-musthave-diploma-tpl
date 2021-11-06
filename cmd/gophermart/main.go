771e784cee4dc503189b57071b90d7fb314a5455
pl_chkn <chpi@liis.su>
2021-11-06 11:34:02 +0300

    add some logs for tests

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"github.com/p7chkn/go-musthave-diploma-tpl/cmd/gophermart/configurations"
 	"github.com/p7chkn/go-musthave-diploma-tpl/internal/workers"
 	"log"
@@ -17,16 +18,24 @@ import (
 )
 
 func main() {
+
+	fmt.Println("Starting server")
 	ctx, cancel := context.WithCancel(context.Background())
 
+	fmt.Println("Starting parse configuration")
 	cfg := configurations.New()
 
+	fmt.Println("Finish parse configurations, starting connection to db")
 	db, err := sql.Open("postgres", cfg.DataBase.DataBaseURI)
+	fmt.Println("Finish db connection")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println("Starting setup db")
 	services.MustSetupDatabase(ctx, db)
 
+	fmt.Println("Finish setup db")
 	wp := workers.New(ctx, cfg.WorkerPool.NumOfWorkers, cfg.WorkerPool.PoolBuffer)
 
 	go func() {
--- cmd/gophermart/services/setupDB.go
+++ cmd/gophermart/services/setupDB.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"log"
 	"path/filepath"
 	"runtime"
@@ -11,6 +12,7 @@ import (
 )
 
 func MustSetupDatabase(ctx context.Context, db *sql.DB) {
+	fmt.Println("Enter a migrations start")
 	_, b, _, _ := runtime.Caller(0)
 	basePath := filepath.Dir(b)
 	migrationsPath := basePath + "/migrations"
