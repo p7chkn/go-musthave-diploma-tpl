02e9344409fb4c7337bfe78f740d1eca71d1bcba
pl_chkn <chpi@liis.su>
2021-11-13 12:27:05 +0300

    remove goose migrations

--- cmd/gophermart/services/setupDB.go
+++ cmd/gophermart/services/setupDB.go
@@ -3,19 +3,8 @@ package services
 import (
 	"database/sql"
 	"go.uber.org/zap"
-	"path/filepath"
-	"runtime"
-
-	"github.com/pressly/goose"
 )
 
 func MustSetupDatabase(db *sql.DB, log *zap.SugaredLogger) {
-	log.Info("Enter a migrations start")
-	_, b, _, _ := runtime.Caller(0)
-	basePath := filepath.Dir(b)
-	migrationsPath := basePath + "/migrations"
-	err := goose.Up(db, migrationsPath)
-	if err != nil {
-		log.Fatal(err)
-	}
+
 }
